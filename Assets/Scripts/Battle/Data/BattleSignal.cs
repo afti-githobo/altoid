fab643255c9f1e0a718a0efffc9b99551b8ba6bb
sine/cosine/tangent <zfshmn@gmail.com>
2023-03-27 02:18:58 -0700

    Starting on action structure and execution

---
+++ Assets/Scripts/Battle/Data/BattleSignal.cs
@@ -0,0 +1,10 @@
+﻿namespace Altoid.Battle.Data
+{
+    public enum BattleSignal
+    {
+        Invalid = -1,
+        None = 0,
+        ActorGenericAtkAnim = 1,
+        TargetGenericHitAnim = 1001
+    }
+}
